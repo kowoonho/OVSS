fd372a7ac39185d9acb58f725db91ddd50084fdb
kowoonho <heesuk9898@gmail.com>
2024-02-22 10:22:35 +0900

    commit

--- tools/dist_launch.sh
+++ tools/dist_launch.sh
@@ -9,7 +9,7 @@
 #
 # Written by Jiarui Xu
 # -------------------------------------------------------------------------
-# export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=4
 # export OMP_NUM_THREADS=2
 
 SCRIPT=$1
